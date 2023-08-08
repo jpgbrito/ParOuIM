1ba478aaa8801fc4922384f03a1e3f16c8fab363
jpgbrito <139816968+jpgbrito@users.noreply.github.com>
2023-08-07 21:01:37 -0300

    Add files via upload

---
+++ ParOuImpar.js
@@ -0,0 +1,19 @@
+
+function vercarParOuImpar() {
+    var numero = parseInt (prompt ("Digite o número:"))
+
+    if (numero % 2 == 0) {
+
+        alert (numero + "é um número par");
+
+        
+
+    } else {
+
+        alert (numero + "é um número ímpar");
+
+    }
+
+}
+
+vercarParOuImpar();
